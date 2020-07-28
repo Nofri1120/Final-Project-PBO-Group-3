c9721682dde4cd4b1042c316da6b5d2ac95f4056
Nofri1120 <64509790+Nofri1120@users.noreply.github.com>
2020-07-29 06:41:34 +0700

    Final Project PBO - Group 3

---
+++ src/java/Math.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+/**
+ *
+ * @author 1817101438
+ */
+public class Math {
+    
+}
